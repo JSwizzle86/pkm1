2f360d34fec11d29d563a39dc22d3822655f15dd
Queue Branch <163742575+QuinnQueue@users.noreply.github.com>
2024-07-22 03:44:17 -0500

    Update BattleDexScene.gd
    You didn't see anything.

--- Dex_Scene_Stuff/Scripts/BattleDexScene.gd
+++ Dex_Scene_Stuff/Scripts/BattleDexScene.gd
@@ -28,22 +28,22 @@ func create_menus():
 	var d = dex_selection_scene.instantiate()
 	add_child(d)
 	d.global_position = global_position
-	visible = true
 	menu_sequence.append(d)
+	d.visible = false
 	print(menu_sequence)
 	
 	var b = battle_data_scene.instantiate()
-	add_child(d)
-	d.global_position = global_position
-	visible = false
+	add_child(b)
+	b.global_position = global_position
 	menu_sequence.append(b)
+	b.visible = false
 	
 	var m = map_scene.instantiate()
-	add_child(d)
-	d.global_position = global_position
+	add_child(m)
+	m.global_position = global_position
 	menu_sequence.append(m)
-	visible = false
 	move_child(color_rect, get_children().size()-1)
+	m.visible = false
 
 func _unhandled_input(event):
 	pass
@@ -51,7 +51,9 @@ func _unhandled_input(event):
 func open_menu():
 	scene_transition.play("Open")
 	await scene_transition.animation_finished
+	menu_sequence.front().visible = true
 	scene_transition.play("Close")
+
 
 func close_menu():
 	pass
