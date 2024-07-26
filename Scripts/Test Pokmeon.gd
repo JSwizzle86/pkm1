c160a52eaeb7ec240f3e74d92943a1add8ae55dc
JSwizzle86 <83131126+JSwizzle86@users.noreply.github.com>
2024-07-25 19:49:33 -0500

    AAAAAAAAAAA fixed

--- Scripts/Test Pokmeon.gd
+++
@@ -1,3 +0,0 @@
-extends Node2D
-
-@export var dex_data = Pokemon_Dex_Data.new()
--- Scripts/pokemon_species_test.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-
-class_name Pokemon_Data
-
-@export var pokemon_dex_data : Pokemon_Dex_Data = Pokemon_Dex_Data.new()
