e8c53cddd527eb13242e546056009c400226c0e7
GamplaxPramgrammer <54183631+GamplaxPramgrammer@users.noreply.github.com>
2025-03-15 21:53:33 -0500

    UID updates plus menu fix
    Allows Aaisu to playtest n stuff (adds the uids needed)

--- Dex_Scene_Stuff/Scripts/PKM1Player.gd
+++ Dex_Scene_Stuff/Scripts/PKM1Player.gd
@@ -2,7 +2,7 @@ class_name Player extends CharacterBody2D
 
 @export var movement_state : MiniState
 @export var current_location : Node2D
-
+@onready var state_machine: StateMachine = $StateMachine
 
 func _ready() -> void:
 	%StateMachine.movement_direction = Global.player_enter_room_direction
--- Main/PauseScene/menu.gd
+++ Main/PauseScene/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var select_arrow = $NinePatchRect/TextureRect
 @onready var menu = $"."
-@onready var player = $"PlayerHolder/PKM!Player"
+@onready var player: Player = $"/root/Main/PlayerHolder/PKM!Player"
 
 enum ScreenLoaded {NOTHING, JUST_MENU, DEX, USERINFO, INVENTORY, SAVE, SETTINGS, EXIT}
 var screen_loaded = ScreenLoaded.NOTHING
@@ -15,28 +15,29 @@ var row = selected_option / columns
 func _ready():
 	grab_focus()
 	menu.visible = false
-	select_arrow.position.y = row * 22 + (22 - 41) / 2
+	updateArrowPosition()
 
 	
 func _process(delta):
 	match screen_loaded:
 		ScreenLoaded.NOTHING:
 			if Input.is_action_just_pressed("pause"):
-				print(player)
-				if !player.is_moving():	
-					player._process(false)
+				if player.state_machine.current_state == player.state_machine.State.Idle:
+					player.set_process(false)
+					player.set_physics_process(false)
 					menu.visible = true
 					screen_loaded = ScreenLoaded.JUST_MENU
 				
 		ScreenLoaded.JUST_MENU:
 			if Input.is_action_just_pressed("pause") or Input.is_action_pressed("Back"):
-				player._process(true)
+				player.set_process(true)
+				player.set_physics_process(true)
 				menu.visible = false
 				screen_loaded = ScreenLoaded.NOTHING
 			elif Input.is_action_pressed("ui_down"):
 				selected_option += 1
 				row = selected_option / columns
-				select_arrow.position.y = row * 22 + (22 - 41) / 2
+				updateArrowPosition()
 			elif Input.is_action_pressed("ui_up"):
 				if selected_option == 0:
 					selected_option = 6
@@ -44,6 +45,7 @@ func _process(delta):
 				else:
 					selected_option -= 1
 					row = selected_option / columns
-				select_arrow.position.y = row * 22 + (22 - 41) / 2
+				updateArrowPosition()
 	
-	
+func updateArrowPosition():
+	select_arrow.position.y = row * 22 + (22 - 41) / 2
