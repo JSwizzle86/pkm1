e7ed0899f4c589d3845abd35a291e3fac8f2af0b
FLGZ <150631107+FLGZ-dev@users.noreply.github.com>
2025-06-26 07:46:03 -0500

    Implemented steppable tile functionality to GridMovement
    Movement will detect if the moving actor steps on a steppable tile and call that tile's on_step()

--- Assets/Actors/ActorTools/Scripts/grid_movement.gd
+++ Assets/Actors/ActorTools/Scripts/grid_movement.gd
@@ -1,7 +1,7 @@
 class_name GridMovement extends Node2D
 
 ##parent node
-@export var self_node: Node2D
+@export var linked_tile: GameTile
 ##Walk Speed, the lower the value the higher the speed[br]
 ##default: 25
 @export var walkSpeed: float = 25
@@ -11,7 +11,7 @@ class_name GridMovement extends Node2D
 
 var moving_direction: Vector2 = Vector2.ZERO
 var diagonal: bool = false
-var collided = false
+var collided: bool = false
 
 func _ready():
 	# Set movement direction as DOWN by default
@@ -68,25 +68,32 @@ func move(direction: Vector2, magnitude: int) -> void:
 			var areas = collidedArea.get_overlapping_areas()
 			if collidedArea.collision:
 				collided = true
-				collidedArea.on_collision(self_node)
+				collidedArea.on_collision(linked_tile)
+			elif collidedArea.steppable:
+				print("stepped")
+				collidedArea.on_step(linked_tile)
 			for area in areas:
 				if is_instance_of(area, GameTile) && area.global_position == collidedArea.global_position:
 					if area.collision: 
 						collided = true
-						area.on_collision(self_node)
+						area.on_collision(linked_tile)
+					elif area.steppable:
+						print("stepped")
+						area.on_step(linked_tile)
 		
 		# Allow movement only if no collision in next tile
 		if magnitude > 0 && !collided:
 			moving_direction = movement * magnitude
-			var new_position = self_node.global_position + (moving_direction * Constants.TILE_SIZE)
+			var new_position = linked_tile.global_position + (moving_direction * Constants.TILE_SIZE)
 			var tween = create_tween()
 			var speed = runSpeed/100 * 2 if magHold > 1 else walkSpeed/100
 			
 			#Keep speed constant in different movement modes
 			if magHold > magnitude:
-				tween.tween_property(self_node, "position", new_position, speed / magHold if !diagonal else speed / magHold / (sqrt(2)/2)).set_trans(Tween.TRANS_LINEAR)
+				tween.tween_property(linked_tile, "position", new_position, speed / magHold if !diagonal else speed / magHold / (sqrt(2)/2)).set_trans(Tween.TRANS_LINEAR)
 			else:
-				tween.tween_property(self_node, "position", new_position, speed if !diagonal else speed / (sqrt(2)/2)).set_trans(Tween.TRANS_LINEAR)
+				tween.tween_property(linked_tile, "position", new_position, speed if !diagonal else speed / (sqrt(2)/2)).set_trans(Tween.TRANS_LINEAR)
 			tween.tween_callback(func(): moving_direction = Vector2.ZERO)
+			
 	diagonal = false
 	collided = false
